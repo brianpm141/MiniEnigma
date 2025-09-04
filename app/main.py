ad9e06b3c2a2a3e377986789dd12ce4a37099119
pmbrian <pmbrian@hotmail.com>
2025-09-03 23:31:39 -0600

    despliegue_conf

--- app/main.py
+++ app/main.py
@@ -1,15 +1,7 @@
-import os
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from .routers.router import router
+import os
 
-
-#creacion de instancia principal
-app = FastAPI(
-    title="MiniEnigma",
-    description="Api de encriptacion de mensajeria",
-    version="0.1.0",
-)
+app = FastAPI()
 
 # Configuracion de CORS para desarrollo y produccion en Vercel
 # Obtener los orígenes permitidos de una variable de entorno
@@ -32,20 +24,6 @@ if not origins:
 
 print(f"CORS Allowed Origins: {origins}") # Added for debugging
 
-# Añade el middleware de CORS a la aplicacion
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-#Coneccion del router
-app.include_router(router)
-
-
 @app.get("/")
 def read_root():
-    return {"message": "Bienvenido a la API de MiniEnigma"}
+    return {"message": "Hello from MiniEnigma (Simplified)!"}
